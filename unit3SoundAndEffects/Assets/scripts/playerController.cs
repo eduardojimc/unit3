a20b5ad4543e35a05fd97dabef4ff85e558aa746
eduardo jc <jibbydotjpeg@gmail.com>
2023-04-20 13:42:47 -0400

    3.1 finished
    finally worked lol

---
+++ unit3SoundAndEffects/Assets/scripts/playerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float jumpForce =10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
---
+++ unit3SoundAndEffects/Assets/scripts/spawnManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour
+{
+    public GameObject obsticlePrefab;
+    private Vector3 spawnPos = new Vector3(25,0,0);
+    private float startDelay =2;
+    private float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnObsticle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void spawnObsticle()
+    { Instantiate(obsticlePrefab, spawnPos, obsticlePrefab.transform.rotation); }
+}
